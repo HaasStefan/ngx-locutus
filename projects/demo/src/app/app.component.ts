e80a8379c7274118854cc279021ed5ea999a0fb0
Stefan Haas <stefan.haas@iib.at>
2022-05-22 18:46:54 +0200

    in-code translation example

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LocutusService } from 'projects/ngx-locutus/src/lib/services/locutus.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'demo';
+export class AppComponent implements OnInit {
+  title$ = new Observable<string>();
+  message!: string;
 
   constructor(private locutus: LocutusService) {}
+
+  ngOnInit(): void {
+    this.title$ = this.locutus.translate('scope1', 'title');
+
+    // avoid this:
+    // because the translation might not be registered yet
+    // it even will throw an error in this demo because exactly that is the case!
+    this.message = this.locutus.instant('scope1', 'message');
+  }
 
   changeLang() {
     let newLanguage = this.locutus.getActiveLanguage() == 'de' ? 'en' : 'de';
